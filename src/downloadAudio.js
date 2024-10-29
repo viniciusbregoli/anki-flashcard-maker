5edc021d99c22800099b62e9357a7c8c5f2c08da
Vinicius Bregoli <viniciusbregoli@hotmail.com>
2024-10-29 13:09:43 +0100

    first commit

---
+++ src/downloadAudio.js
@@ -0,0 +1,36 @@
+import fetch from "node-fetch";
+import fs from "fs";
+import { parseStringPromise } from "xml2js";
+import dotenv from "dotenv";
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+// Function to download MP3 pronunciation for a given word
+export async function downloadPronunciation(word, language = "de", id) {
+    try {
+        // Define the API endpoint with dynamic word and language
+        const apiUrl = `https://apifree.forvo.com/key/${apiKey}/format/xml/action/standard-pronunciation/word/${word}/language/${language}`;
+
+        // Fetch the XML response
+        const response = await fetch(apiUrl);
+        const xmlString = await response.text();
+
+        // Parse the XML data
+        const result = await parseStringPromise(xmlString);
+
+        // Extract the first pathmp3 element
+        const pathMp3 = result.items.item[0].pathmp3[0];
+
+        // Fetch the MP3 file
+        const mp3Response = await fetch(pathMp3);
+        const data = await mp3Response.arrayBuffer();
+
+        // Write the MP3 file to the local filesystem
+        fs.writeFileSync(`C:/Users/Vinic/AppData/Roaming/Anki2/User 1/collection.media/${word}_pronunciation.mp3`, Buffer.from(data));
+
+        console.log(`MP3 file for card ${id} downloaded successfully!`);
+    } catch (error) {
+        console.error("Error downloading pronunciation, no audio available");
+    }
+}
+
---
+++ src/reverso.js
@@ -0,0 +1,94 @@
+import fs from "fs";
+import Reverso from "reverso-api";
+import { downloadPronunciation } from "./downloadAudio.js";
+
+const writeStream = fs.createWriteStream("./src/output.txt");
+let linesArray = [];
+let cards = [];
+const reverso = new Reverso();
+
+try {
+    const data = fs.readFileSync("./src/test.txt", "utf8");
+    linesArray = data.split("\r\n");
+    console.log(linesArray);
+} catch (err) {
+    console.error(err);
+}
+
+processLines().catch((error) => {
+    console.error("Error processing cards:", error);
+});
+
+async function processLines() {
+    for (let i = 0; i < linesArray.length; i++) {
+        await processLine(capitalizeString(linesArray[i]), i); // Await each line processing to ensure it's synchronous
+    }
+
+    cards.forEach((card) => {
+        // Check if the context array has at least one element
+        if (card.context.length > 0) {
+            writeStream.write(
+                `[sound:${card.source.replace(/\s+/g, "_")}_pronunciation.mp3] ${
+                    card.source
+                } <br> <i>${card.context[0]["german"]}</i>; ${card.translation.join(", ")} <br> <i>${card.context[0]["english"]}</i> ` + "\n"
+            );
+        } else {
+            writeStream.write(
+                `[sound:${card.source.replace(/\s+/g, "_")}_pronunciation.mp3] ${
+                    card.source
+                } <br> ${card.translation.join(", ")} ` + "\n"
+            );
+        }
+    });
+}
+
+async function processLine(line, index) {
+    let context = [];
+    let translation = [];
+    let attempt = 0;
+    let maxRetries = 5;
+    let success = false;
+
+    while (attempt < maxRetries && !success) {
+        try {
+            const response = await reverso.getTranslation(line.toLowerCase(), "german", "english");
+            await downloadPronunciation(line.replace(/\s+/g, "_"), "de", index);
+            if (!response.translations) {
+                console.error(`Invalid response for line ${(index, line)}: missing translations, trying again...`);
+                attempt++;
+                continue;
+            } else {
+                translation.push(...response.translations.slice(1, 4));
+                console.log(`Successfull translation for word ${line}`);
+                success = true;
+            }
+            if (response.context && response.context.examples) {
+                for (let j = 0; j < 3; j++) {
+                    if (response.context.examples[j]) {
+                        context.push({
+                            english: response.context.examples[j]["target"].replace(/;/g, ","),
+                            german: response.context.examples[j]["source"].replace(/;/g, ","),
+                        });
+                    }
+                }
+                let newCard = {
+                    id: index,
+                    source: line,
+                    translation: translation,
+                    context: context,
+                };
+                cards.push(newCard);
+            } else {
+                console.error(`Invalid response for line ${index}: missing examples`);
+            }
+        } catch (error) {
+            console.error(`Error processing line ${index}:`, error);
+            attempt++;
+        }
+    }
+}
+
+function capitalizeString(str) {
+    if (!str) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
